b1fda63d79bc6e9687c3a156943a5daec9560049
Promethee Toneatti <promethee.toneatti@telecom-sudparis.eu>
2022-12-05 17:36:49 +0100

    before remove tasklist local

--- app/src/main/java/com/alexprom/app/tasklist/TasksListViewModel.kt
+++ app/src/main/java/com/alexprom/app/tasklist/TasksListViewModel.kt
@@ -23,11 +23,40 @@ class TasksListViewModel : ViewModel() {
             tasksStateFlow.value = fetchedTasks // on modifie le flow, ce qui déclenche ses observers
         }
     }
+    
+    fun add(task: Task) {
+        viewModelScope.launch {
+            val response = webService.create(task)
+            if (!response.isSuccessful) {
+                Log.e("Network", "Error: ${response.raw()}")
+                return@launch
+            }
+            val newTask = response.body()!!
+            tasksStateFlow.value = tasksStateFlow.value + newTask
+        }
+    }
+    fun remove(task: Task) {
+        viewModelScope.launch {
+            val response = webService.delete(task)
+            if (!response.isSuccessful) {
+                Log.e("Network", "Error: ${response.raw()}")
+                return@launch
+            }
+            tasksStateFlow.value = tasksStateFlow.value - task
+        }
+    }
+    fun edit(task: Task) {
+        viewModelScope.launch {
+            val response = webService.update(task)
+            if (!response.isSuccessful) {
+                Log.e("Network", "Error: ${response.raw()}")
+                return@launch
+            }
 
-    // à compléter plus tard:
-    fun add(task: Task) {}
-    fun edit(task: Task) {}
-    fun remove(task: Task) {}
+            val updatedTask = response.body()!!
+            tasksStateFlow.value = tasksStateFlow.value.map { if (it.id == updatedTask.id) updatedTask else it }
+        }
+    }
 
 
 }
